a38fe50df203f192df0fec8511867d2365fd2717
Bardoctorus <ian.m.buckley@gmail.com>
2023-10-22 14:20:12 +0200

    Added a bit to readme about the letter matchin nonsense

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 * TODO: This readme.
 
 Will be Rust parser type thing not dissimilar to the [EM Parser]("https://github.com/Bardoctorus/EMparser") I made with some updates and with more Rust goodness
+
+AS it stands however this is a bit of a playground for jsut learning a few Rust concepts. For example, I'm currently working on something that'll check the first letters of each word against a vector of letters, with the idea of counting them. For no reason at all.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::env;
 
 
+struct StartingLetter {
+    starts_with : String, 
+    count : i32,
+    words : Vec<String>
+}
+
 fn main() {
+    let letters : Vec<&str> = ["a","b","c","d"].to_vec();
     let args: Vec<String> = env::args().collect();
     let mut together : String = String::new();
     // Iterate over the args ignoring the first, adding a space unless it's the end
@@ -9,12 +16,15 @@ fn main() {
         if i == 0 {continue;}
         if i+1 == args.len(){
             println!("hit end of ting");
-            together.push_str(v);
+           together.push_str(v);
             break;
         };
         together.push_str(v);
         together.push_str(" ");
+        println!("v in upper: {} and lower: {}",v[0..1].to_uppercase(), v[0..1].to_lowercase());
+        if &v[..1].to_lowercase() == letters[0] {println!("First letter of {} matches with {}", v, letters[0])};
     }
 
     println!("full string is {}", together);
+    
 }
