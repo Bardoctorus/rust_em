388c9ac5310a05ed0e1291f07174e4fb43dd982a
Bardoctorus <ian.m.buckley@gmail.com>
2023-10-22 12:44:45 +0200

    test output, fix repeated word and extra space bug

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-//    dbg!(args);
     let mut together : String = String::new();
+    // Iterate over the args ignoring the first, adding a space unless it's the end
     for (i, v) in args.iter().enumerate() {
-        if i == 0 {
-        continue;
-        }
-        if i+1 > args.len(){
-        together.push_str(v);
-        }
+        if i == 0 {continue;}
+        if i+1 == args.len(){
+            println!("hit end of ting");
+            together.push_str(v);
+            break;
+        };
         together.push_str(v);
         together.push_str(" ");
     }
