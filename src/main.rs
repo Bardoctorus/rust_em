1ca184ad6c67c1a58e64b623a6cfc84e7751ec52
Bardoctorus <ian.m.buckley@gmail.com>
2023-10-23 15:24:30 +0200

    Thought I'd committed, more letter dinks

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct StartingLetter {
 }
 
 fn main() {
-    let letters : Vec<&str> = ["a","b","c","d"].to_vec();
+    let letters : Vec<&str> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].to_vec();
     let args: Vec<String> = env::args().collect();
     let mut together : String = String::new();
     // Iterate over the args ignoring the first, adding a space unless it's the end
@@ -21,8 +21,14 @@ fn main() {
         };
         together.push_str(v);
         together.push_str(" ");
-        println!("v in upper: {} and lower: {}",v[0..1].to_uppercase(), v[0..1].to_lowercase());
-        if &v[..1].to_lowercase() == letters[0] {println!("First letter of {} matches with {}", v, letters[0])};
+//        println!("v in upper: {} and lower: {}",v[0..1].to_uppercase(), v[0..1].to_lowercase());
+        for i in 0..letters.len(){
+            if &v[..1].to_lowercase() == letters[i] {println!("First letter of {} matches with {}", v, letters[i])};
+        }
+        for i in 0..letters.len(){
+        println!("letter {} is {}",i, letters[i]);
+        
+        }
     }
 
     println!("full string is {}", together);
