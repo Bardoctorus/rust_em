8148482fce9703bcedc6ff078b1a3f8a6a7e19f4
Bardoctorus <ian.m.buckley@gmail.com>
2023-10-24 17:27:05 +0200

    moved all of the non-relevant code out. So all of it. Start again

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Will be Rust parser type thing not dissimilar to the [EM Parser]("https://github.com/Bardoctorus/EMparser") I made with some updates and with more Rust goodness
 
-AS it stands however this is a bit of a playground for jsut learning a few Rust concepts. For example, I'm currently working on something that'll check the first letters of each word against a vector of letters, with the idea of counting them. For no reason at all.
+I had to nuke it all as it was lots of things, none of which were the parser. All of the code that was in it is now in the unforunately named text_anal project.
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,6 @@
-use std::env;
-
-
-struct StartingLetter {
-    starts_with : String, 
-    count : i32,
-    words : Vec<String>
+fn main(){
+println!("this is now empty again so you can make it actuall the parser you knob");
 }
 
-fn main() {
-    let letters : Vec<&str> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].to_vec();
-    let args: Vec<String> = env::args().collect();
-    let mut together : String = String::new();
-    // Iterate over the args ignoring the first, adding a space unless it's the end
-    for (i, v) in args.iter().enumerate() {
-        if i == 0 {continue;}
-        if i+1 == args.len(){
-            println!("hit end of ting");
-           together.push_str(v);
-            break;
-        };
-        together.push_str(v);
-        together.push_str(" ");
-//        println!("v in upper: {} and lower: {}",v[0..1].to_uppercase(), v[0..1].to_lowercase());
-        for i in 0..letters.len(){
-            if &v[..1].to_lowercase() == letters[i] {println!("First letter of {} matches with {}", v, letters[i])};
-        }
-        for i in 0..letters.len(){
-        println!("letter {} is {}",i, letters[i]);
-        
-        }
-    }
 
-    println!("full string is {}", together);
-    
-}
+
